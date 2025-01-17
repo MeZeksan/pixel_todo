cc5c30e96ed4128bba1897ba06a1688351a0f6a7
MeZeksan <bulzak74848@gmail.com>
2025-01-17 20:16:09 +0500

    Изменил цвет досок с помощью colorFilter

--- lib/features/home/view/home_screen.dart
+++ lib/features/home/view/home_screen.dart
@@ -57,12 +57,14 @@ class HomeScreen extends StatelessWidget {
               child: Container(
                 decoration: const BoxDecoration(
                   image: DecorationImage(
-                    alignment: Alignment.topLeft,
-                    image:
-                        AssetImage('assets/images/wooden_background_desk.png'),
-                    fit: BoxFit.contain,
-                    repeat: ImageRepeat.repeat, // Повторение фона
-                  ),
+                      alignment: Alignment.topLeft,
+                      image: AssetImage(
+                          'assets/images/wooden_background_desk.png'),
+                      fit: BoxFit.contain,
+                      repeat: ImageRepeat.repeat, // Повторение фона
+                      colorFilter: ColorFilter.mode(
+                          Color.fromARGB(255, 211, 183, 122),
+                          BlendMode.overlay)),
                 ),
                 child: const CustomScrollView(
                   slivers: [TasksList()],
