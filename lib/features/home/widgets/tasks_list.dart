41d507b59242e597ccb1c01cf3bc821d3a463169
MeZeksan <bulzak74848@gmail.com>
2025-01-31 19:37:37 +0500

    Сортировка по приоритету. Внутри приоритета по дате

--- lib/features/home/widgets/tasks_list.dart
+++ lib/features/home/widgets/tasks_list.dart
@@ -36,9 +36,7 @@ class _SliverTasksState extends State<TasksList> {
           );
         }
 
-        // Сортировка по приоритету
-        final List<Task> tasks = box.values.toList()
-          ..sort((a, b) => b.priority.compareTo(a.priority));
+        final List<Task> tasks = sortTasksByPriorityAndDueDate(box);
 
         return SliverList(
           delegate: SliverChildBuilderDelegate(
@@ -52,4 +50,29 @@ class _SliverTasksState extends State<TasksList> {
       },
     );
   }
+}
+
+// Сортировка по приоритету и внутри приоритета по дате
+List<Task> sortTasksByPriorityAndDueDate(Box<Task> box) {
+  final List<Task> sortedTask = box.values.toList()
+    ..sort((a, b) {
+      // Сначала сортируем по приоритету в порядке убывания
+      final priorityComparison = b.priority.compareTo(a.priority);
+      if (priorityComparison != 0) {
+        return priorityComparison;
+      }
+
+      // Если приоритет одинаковый, сортируем по дате
+      if (a.dueDate != null && b.dueDate != null) {
+        return a.dueDate!.compareTo(b.dueDate!);
+      } else if (a.dueDate != null) {
+        return -1; // Задачи с установленной датой идут перед задачами без даты
+      } else if (b.dueDate != null) {
+        return 1; // Задачи без даты идут после задач с установленной датой
+      }
+
+      return 0; // Если обе задачи не имеют даты, оставляем их в текущем порядке
+    });
+
+  return sortedTask;
 }
