0b2b6cfb1d30c83040fc5b7a4cf66a5a3dee60d6
MeZeksan <bulzak74848@gmail.com>
2025-02-03 16:09:12 +0500

    Прописал макет Bloc для home_screen

--- lib/features/home/bloc/home_screen.bloc.dart
+++ lib/features/home/bloc/home_screen.bloc.dart
@@ -1,6 +1,17 @@
 import 'package:bloc/bloc.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:pixel_todo/models/task/task.dart';
 
 part 'home_screen_event.bloc.dart';
 part 'home_screen_state.bloc.dart';
 
-class HomeBloc extends Bloc<HomeEvent, HomeState> {}
+class HomeBloc extends Bloc<HomeEvent, HomeState> {
+  //не сделал через GetIt, чтобы нужно было явно передавать бокс
+  final Box<Task> _taskBox;
+
+  HomeBloc(this._taskBox) : super(HomeInitial()) {
+    on<LoadTasks>(_onLoadTasks);
+    on<AddTask>(_onAddTasks);
+    on<DeleteTask>(_onDeleteTasks);
+  }
+}
