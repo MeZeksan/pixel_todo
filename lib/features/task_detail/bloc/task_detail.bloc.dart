6d663bed6e94fda87fa4915025195a9efe26be41
MeZeksan <bulzak74848@gmail.com>
2025-02-05 21:26:28 +0500

    Прописал стейты

--- lib/features/task_detail/bloc/task_detail.bloc.dart
+++ lib/features/task_detail/bloc/task_detail.bloc.dart
@@ -1,2 +1,4 @@
+import 'package:pixel_todo/models/task/task.dart';
+
 part 'task_detail_event.bloc.dart';
 part 'task_detail_state.bloc.dart';
--- lib/features/task_detail/bloc/task_detail_state.bloc.dart
+++ lib/features/task_detail/bloc/task_detail_state.bloc.dart
@@ -1,1 +1,13 @@
 part of 'task_detail.bloc.dart';
+
+abstract class TaskDetailState {}
+
+class TaskDetailInitial extends TaskDetailState {
+  final Task task;
+  TaskDetailInitial(this.task);
+}
+
+class TaskDetailUpdate extends TaskDetailState {
+  final Task updatedTask;
+  TaskDetailUpdate(this.updatedTask);
+}
