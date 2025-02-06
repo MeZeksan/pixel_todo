9884bd1ff41b1572402ea276c341d67555fa4049
MeZeksan <bulzak74848@gmail.com>
2025-02-06 19:42:45 +0500

    Изменение логики сервиса сортировки. Добавление класса и статического метода

--- lib/features/home/service/task_sort.service.dart
+++ lib/features/home/service/task_sort.service.dart
@@ -1,26 +1,28 @@
 // Сортировка по приоритету и внутри приоритета по дате
 import 'package:pixel_todo/models/task/task.dart';
 
-List<Task> sortTasksByPriorityAndDueDate(List<Task> tasks) {
-  final List<Task> sortedTask = tasks
-    ..sort((a, b) {
-      // Сначала сортируем по приоритету в порядке убывания
-      final priorityComparison = b.priority.compareTo(a.priority);
-      if (priorityComparison != 0) {
-        return priorityComparison;
-      }
+class TaskSortService {
+  static List<Task> sortTasksByPriorityAndDueDate(List<Task> tasks) {
+    final List<Task> sortedTask = tasks
+      ..sort((a, b) {
+        // Сначала сортируем по приоритету в порядке убывания
+        final priorityComparison = b.priority.compareTo(a.priority);
+        if (priorityComparison != 0) {
+          return priorityComparison;
+        }
 
-      // Если приоритет одинаковый, сортируем по дате
-      if (a.dueDate != null && b.dueDate != null) {
-        return a.dueDate!.compareTo(b.dueDate!);
-      } else if (a.dueDate != null) {
-        return -1; // Задачи с установленной датой идут перед задачами без даты
-      } else if (b.dueDate != null) {
-        return 1; // Задачи без даты идут после задач с установленной датой
-      }
+        // Если приоритет одинаковый, сортируем по дате
+        if (a.dueDate != null && b.dueDate != null) {
+          return a.dueDate!.compareTo(b.dueDate!);
+        } else if (a.dueDate != null) {
+          return -1; // Задачи с установленной датой идут перед задачами без даты
+        } else if (b.dueDate != null) {
+          return 1; // Задачи без даты идут после задач с установленной датой
+        }
 
-      return 0; // Если обе задачи не имеют даты, оставляем их в текущем порядке
-    });
+        return 0; // Если обе задачи не имеют даты, оставляем их в текущем порядке
+      });
 
-  return sortedTask;
+    return sortedTask;
+  }
 }
--- lib/features/home/widgets/tasks_list.dart
+++ lib/features/home/widgets/tasks_list.dart
@@ -39,7 +39,7 @@ class _SliverTasksState extends State<TasksList> {
 
           // Сортируем задачи
           final List<Task> sortedTasks =
-              sortTasksByPriorityAndDueDate(tasksList);
+              TaskSortService.sortTasksByPriorityAndDueDate(tasksList);
 
           // Возвращаем список задач
           return SliverList(
