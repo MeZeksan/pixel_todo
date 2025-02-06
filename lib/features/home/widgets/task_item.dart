b4f963a9d4aa41e7e5fa9a53ecc055113a5902d3
MeZeksan <bulzak74848@gmail.com>
2025-02-06 19:22:25 +0500

    После того, как изменения будут приняты - перезагрузка списка задач

--- lib/features/home/widgets/task_item.dart
+++ lib/features/home/widgets/task_item.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hive_flutter/hive_flutter.dart';
+import 'package:pixel_todo/features/home/bloc/home_screen.bloc.dart';
 import 'package:pixel_todo/features/home/widgets/widgets.dart';
 import 'package:pixel_todo/features/task_detail/view/view.dart';
 import 'package:pixel_todo/models/task/task.dart';
@@ -11,37 +13,44 @@ class TaskItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-      child: GestureDetector(
-        onTap: () {
-          Navigator.push(
-            context,
-            MaterialPageRoute(
-              builder: (context) => TaskDetailsScreen(
-                task: task,
-                taskBox: taskBox,
+    return BlocConsumer<HomeBloc, HomeState>(listener: (context, state) {
+      // Здесь можно обработать изменения состояния, если нужно
+    }, builder: (context, state) {
+      return Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+        child: GestureDetector(
+          onTap: () async {
+            // Ожидаем возврата из TaskDetailsScreen
+            final result = await Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => TaskDetailsScreen(
+                  initialTask: task,
+                  taskBox: taskBox,
+                ),
               ),
-            ),
-          );
-        },
-        child: Stack(
-          alignment: Alignment.center,
-          children: [
-            Image.asset(
-              'assets/images/pergament.png',
-              width: double.infinity,
-              height: 100,
-              fit: BoxFit.fill,
-            ),
-            TaskCheckBox(task: task),
-            PriorityDifficultyImage(task: task),
-            TaskLabel(task: task),
-            TaskDate(task: task),
-            TaskDeleteButton(task: task),
-          ],
+            );
+            // Если результат true -обновляем задачи
+            if (result == true) context.read<HomeBloc>().add(LoadTasks());
+          },
+          child: Stack(
+            alignment: Alignment.center,
+            children: [
+              Image.asset(
+                'assets/images/pergament.png',
+                width: double.infinity,
+                height: 100,
+                fit: BoxFit.fill,
+              ),
+              TaskCheckBox(task: task),
+              PriorityDifficultyImage(task: task),
+              TaskLabel(task: task),
+              TaskDate(task: task),
+              TaskDeleteButton(task: task),
+            ],
+          ),
         ),
-      ),
-    );
+      );
+    });
   }
 }
