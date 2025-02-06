e2ae87c19dcd628e1efa8f72fc2c05fcd7d8214b
MeZeksan <bulzak74848@gmail.com>
2025-02-06 19:15:54 +0500

    Небольшое изменение

--- lib/features/task_detail/bloc/task_detail.bloc.dart
+++ lib/features/task_detail/bloc/task_detail.bloc.dart
@@ -1,20 +1,24 @@
-// import 'package:flutter_bloc/flutter_bloc.dart';
-// import 'package:hive_flutter/hive_flutter.dart';
-// import 'package:pixel_todo/models/task/task.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:pixel_todo/models/task/task.dart';
 
-// part 'task_detail_event.bloc.dart';
-// part 'task_detail_state.bloc.dart';
+part 'task_detail_event.bloc.dart';
+part 'task_detail_state.bloc.dart';
 
-// class TaskDetailBloc extends Bloc<TaskDetailEvent, TaskDetailState> {
-//   final Task _task;
-//   final Box<Task> _taskBox;
-//   TaskDetailBloc(this._task, this._taskBox) : super(TaskDetailInitial(task){
-//     on<UpdateTaskTitle> (_onUpdateTaskTitle);
-//     on<UpdateTaskDescription>(_onUpdateTaskDescription);
-//     on<UpdateTaskStatus>(_onUpdateTaskStatus);
-//     on<UpdateTaskPriority>(_onUpdateTaskPriority);
-//     on<UpdateTaskDifficulty>(_onUpdateTaskDifficulty);
-//     on<UpdateTaskDueDate>(onUpdateTaskDueDate);
-//     on<SaveTaskEvent>(_onSaveTask);
-//   });
-// }
+class TaskDetailBloc extends Bloc<TaskDetailEvent, TaskDetailState> {
+  //чтобы не было частых обращений через GetIt - передам бокс
+  final Task initialTask;
+  final Box<Task> taskBox;
+  TaskDetailBloc(this._task, this._taskBox) : super(TaskDetailInitial(task)){
+    //регистрация обработчиков событий
+    on<UpdateTaskTitle> (_onUpdateTaskTitle);
+    on<UpdateTaskDescription>(_onUpdateTaskDescription);
+    on<UpdateTaskStatus>(_onUpdateTaskStatus);
+    on<UpdateTaskPriority>(_onUpdateTaskPriority);
+    on<UpdateTaskDifficulty>(_onUpdateTaskDifficulty);
+    on<UpdateTaskDueDate>(onUpdateTaskDueDate);
+    on<SaveTaskEvent>(_onSaveTask);
+
+
+  };
+}
