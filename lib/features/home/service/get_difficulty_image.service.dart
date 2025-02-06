2f9c2ee7a29b9728f81ac55e1fa4672c4d9e96de
MeZeksan <bulzak74848@gmail.com>
2025-02-06 19:40:06 +0500

    Создание класса для сервиса получения изображения в зависимости от сложности и приоритета. Методы объявлены статическими

--- lib/features/home/service/get_difficulty_image.service.dart
+++ lib/features/home/service/get_difficulty_image.service.dart
@@ -1,15 +1,17 @@
+class GetDifficultyImageService {
 // Функция для выбора картинки в зависимости от приоритета
-String getDiffucultyImage(int difficulty) {
-  switch (difficulty) {
-    case 0:
-      return 'assets/images/easy_difficulty.png';
-    case 1:
-      return 'assets/images/medium_difficulty.png';
-    case 2:
-      return 'assets/images/hard_difficulty.png';
-    case 3:
-      return 'assets/images/insane_difficulty.png';
-    default:
-      return 'assets/images/easy_difficulty.png'; // По умолчанию низкий приоритет
+  static String getDiffucultyImage(int difficulty) {
+    switch (difficulty) {
+      case 0:
+        return 'assets/images/easy_difficulty.png';
+      case 1:
+        return 'assets/images/medium_difficulty.png';
+      case 2:
+        return 'assets/images/hard_difficulty.png';
+      case 3:
+        return 'assets/images/insane_difficulty.png';
+      default:
+        return 'assets/images/easy_difficulty.png'; // По умолчанию низкий приоритет
+    }
   }
 }
--- lib/features/home/service/get_priority_image.service.dart
+++ lib/features/home/service/get_priority_image.service.dart
@@ -1,13 +1,15 @@
-// Функция для выбора картинки в зависимости от приоритета
-String getPriorityImage(int priority) {
-  switch (priority) {
-    case 0:
-      return 'assets/images/low_priority.png'; // Изображение для низкого приоритета
-    case 1:
-      return 'assets/images/medium_priority.png'; // Изображение для среднего приоритета
-    case 2:
-      return 'assets/images/top_priority.png'; // Изображение для высокого приоритета
-    default:
-      return 'assets/images/catbox.png'; // По умолчанию низкий приоритет
+class GetPriorityImageService {
+  static String getPriorityImage(int priority) {
+    // Функция для выбора картинки в зависимости от приоритета
+    switch (priority) {
+      case 0:
+        return 'assets/images/low_priority.png'; // Изображение для низкого приоритета
+      case 1:
+        return 'assets/images/medium_priority.png'; // Изображение для среднего приоритета
+      case 2:
+        return 'assets/images/top_priority.png'; // Изображение для высокого приоритета
+      default:
+        return 'assets/images/catbox.png'; // По умолчанию низкий приоритет
+    }
   }
 }
