165405060b0408c768663221a2c90045b1d0599a
MeZeksan <bulzak74848@gmail.com>
2025-02-16 20:17:46 +0500

    Отрисовка приоритета задачи

--- lib/core/colors/app_colors.dart
+++ lib/core/colors/app_colors.dart
@@ -3,9 +3,9 @@ import 'package:flutter/material.dart';
 class AppColors {
   // Цветовая палитра для уровней сложности
   static const Map<int, Color> difficultyColors = {
-    0: Color.fromARGB(105, 249, 255, 221), // Easy
-    1: Color.fromARGB(105, 221, 255, 225), // Medium
-    2: Color.fromARGB(105, 255, 221, 221), // Hard
+    0: Color.fromARGB(105, 198, 247, 255), // Easy
+    1: Color.fromARGB(105, 210, 255, 215), // Medium
+    2: Color.fromARGB(105, 255, 210, 210), // Hard
     3: Color.fromARGB(105, 226, 197, 255), // Insane
   };
 }
