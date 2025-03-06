a3708cb9f10dc72f3f865523583ae0ffe5c69408
MeZeksan <bulzak74848@gmail.com>
2025-03-06 18:44:44 +0500

    Инициализация проекта

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
 import 'package:hive_flutter/hive_flutter.dart';
+import 'package:pixel_todo/firebase_options.dart';
 
 import 'features/home/home.dart';
 import 'models/task/task.dart';
@@ -9,6 +11,10 @@ const todoBoxName = 'todo_box_name';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
   await Hive.initFlutter(); //инициализация Hive
 
