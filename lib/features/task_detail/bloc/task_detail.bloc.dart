3a57396705bdfad86994e90ce1c009ce3a3cbcfc
MeZeksan <bulzak74848@gmail.com>
2025-03-09 22:16:30 +0500

    Улучшил читаемость кода

--- lib/features/task_detail/bloc/task_detail.bloc.dart
+++ lib/features/task_detail/bloc/task_detail.bloc.dart
@@ -8,16 +8,14 @@ part 'task_detail_event.bloc.dart';
 part 'task_detail_state.bloc.dart';
 
 class TaskDetailBloc extends Bloc<TaskDetailEvent, TaskDetailState> {
-  final Task initialTask; // Начальная задача, которая передается в BLoC
+  final Task initialTask;
 
   // Получаем боксы через GetIt
   final Box<Task> _taskBox = GetIt.I<Box<Task>>(instanceName: todoBoxName);
   final Box<Task> _completedBox =
       GetIt.I<Box<Task>>(instanceName: completedBoxName);
 
-  // Конструктор BLoC
   TaskDetailBloc(this.initialTask) : super(TaskDetailInitial(initialTask)) {
-    // Регистрируем обработчики событий
     on<UpdateTaskTitle>(_onUpdateTaskTitle);
     on<UpdateTaskDescription>(_onUpdateTaskDescription);
     on<UpdateTaskStatus>(_onUpdateTaskStatus);
@@ -26,72 +24,53 @@ class TaskDetailBloc extends Bloc<TaskDetailEvent, TaskDetailState> {
     on<UpdateTaskDueDate>(_onUpdateTaskDueDate);
     on<SaveTaskEvent>(_onSaveTask);
   }
-  // Вспомогательный метод для получения текущей задачи из состояния
-  Task _currentTask(TaskDetailState state) {
-    return state.task; // Используем геттер task из состояния
-  }
 
-  // Обработчик события обновления названия задачи
+  // Геттер для текущей задачи
+  Task get _currentTask => state.task;
+
   void _onUpdateTaskTitle(
       UpdateTaskTitle event, Emitter<TaskDetailState> emit) {
-    final currentTask = _currentTask(state);
-    // Создаем копию задачи с новым названием и эмитим новое состояние
-    emit(TaskDetailUpdated(currentTask.copyWith(taskTitle: event.taskTitle)));
+    // Каждый раз мы создаем копию задачи с новым приоритетом и эмитим новое состояние
+    emit(TaskDetailUpdated(_currentTask.copyWith(taskTitle: event.taskTitle)));
   }
 
-  // Обработчик события обновления описания задачи
   void _onUpdateTaskDescription(
       UpdateTaskDescription event, Emitter<TaskDetailState> emit) {
-    final currentTask = _currentTask(state);
-    // Создаем копию задачи с новым описанием и эмитим новое состояние
     emit(TaskDetailUpdated(
-        currentTask.copyWith(taskDescription: event.taskDescription)));
+        _currentTask.copyWith(taskDescription: event.taskDescription)));
   }
 
-  // Обработчик события обновления статуса задачи (выполнена/не выполнена)
   void _onUpdateTaskStatus(
       UpdateTaskStatus event, Emitter<TaskDetailState> emit) {
-    final currentTask = _currentTask(state);
-    // Создаем копию задачи с новым статусом и эмитим новое состояние
-    emit(TaskDetailUpdated(currentTask.copyWith(isCompleted: event.isChecked)));
+    emit(
+        TaskDetailUpdated(_currentTask.copyWith(isCompleted: event.isChecked)));
   }
 
-  // Обработчик события обновления приоритета задачи
   void _onUpdateTaskPriority(
       UpdateTaskPriority event, Emitter<TaskDetailState> emit) {
-    final currentTask = _currentTask(state);
-    // Создаем копию задачи с новым приоритетом и эмитим новое состояние
-    emit(TaskDetailUpdated(currentTask.copyWith(priority: event.priority)));
+    emit(TaskDetailUpdated(_currentTask.copyWith(priority: event.priority)));
   }
 
-  // Обработчик события обновления сложности задачи
   void _onUpdateTaskDifficulty(
       UpdateTaskDifficulty event, Emitter<TaskDetailState> emit) {
-    final currentTask = _currentTask(state);
-    // Создаем копию задачи с новой сложностью и эмитим новое состояние
-    emit(TaskDetailUpdated(currentTask.copyWith(difficulty: event.difficulty)));
+    emit(
+        TaskDetailUpdated(_currentTask.copyWith(difficulty: event.difficulty)));
   }
 
-  // Обработчик события обновления даты выполнения задачи
   void _onUpdateTaskDueDate(
       UpdateTaskDueDate event, Emitter<TaskDetailState> emit) {
-    final currentTask = _currentTask(state);
-    // Создаем копию задачи с новой датой выполнения и эмитим новое состояние
-    emit(TaskDetailUpdated(currentTask.copyWith(dueDate: event.dueDate)));
+    emit(TaskDetailUpdated(_currentTask.copyWith(dueDate: event.dueDate)));
   }
 
-  // Обработчик события сохранения задачи
   void _onSaveTask(SaveTaskEvent event, Emitter<TaskDetailState> emit) {
-    final currentTask = _currentTask(state);
+    final currentTask = _currentTask;
 
-    // Определяем целевой бокс в зависимости от статуса
+    // Определяем целевой и исходный боксы
     final targetBox = currentTask.isCompleted ? _completedBox : _taskBox;
     final sourceBox = currentTask.isCompleted ? _taskBox : _completedBox;
 
-    // Удаляем из исходного бокса и добавляем в целевой
     sourceBox.delete(currentTask.id);
     targetBox.put(currentTask.id, currentTask);
-
     emit(TaskDetailInitial(currentTask));
   }
 }
